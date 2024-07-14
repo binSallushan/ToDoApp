edbb2d108228935652a9e171d66bd4e523dba6e8
iwamcoding <iwam31023@gmail.com>
2024-07-14 06:44:19 +0300

    Add project files.

---
+++ ToDoAPI/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoAPI.Models
+{
+    public class TodoItem
+    {
+        public static int currentID = 0;
+        public static List<TodoItem> AllItems = new List<TodoItem>();
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
